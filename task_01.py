b97137bcd5fca4336d4f02bf8aea90601899f489
Ruslan <ruslan01082004@gmail.com>
2025-12-23 13:26:25 +0300

    simple decision of first task

--- task_01.py
+++ task_01.py
@@ -3,4 +3,15 @@
 # так и с конца), при условии игнорирования пробелов, знаков препинания и
 # регистра.
 
+def is_palindrome(string):
+    if string == None:
+        return False
+    
+    s = str(string).lower()
+    only_letter = ""
 
+    for i in s:
+        if i.isalnum():
+            only_letter += i
+    
+    return s == s[::-1]
